ba036b5a4afe5a0da53922c842bf7b42d9f36a5a
Zain Ahmad Khan <niazi_zain@hotmail.com>
2020-11-24 19:58:38 +0500

    Fixes to value change emitter, leaves 0

--- projects/ng-number-picker/src/lib/number-picker.component.ts
+++ projects/ng-number-picker/src/lib/number-picker.component.ts
@@ -142,7 +142,7 @@ export class NumberPickerComponent implements OnInit {
     } else if (this.value < this.min) {
       this.value = this.min;
     }
-    if (this.parseVal(this.value)) {
+    valueChange    if (this.parseVal(this.value) === 0 || this.parseVal(this.value)) {
       this.valueChange.emit(this.value);
     }
   }
